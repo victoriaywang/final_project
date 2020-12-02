5fa9aa0719dbd07cdc6736bd8091c188fd72570b
Victoria Wang <victoriawang@Victorias-MacBook-Pro.local>
2020-12-02 03:23:27 -0500

    modeling on shinyapp, police killings data

--- shinyapp/app.R
+++ shinyapp/app.R
@@ -18,6 +18,7 @@ acled_data <- readRDS("acled_data.RDS")
 rural <- readRDS("rural.RDS")
 acled_data_11_6 <- readRDS("acled_data_11_6.RDS")
 acled_data_11_12 <- readRDS("acled_data_11_12.RDS")
+model_12_2 <- readRDS("model_12_2.RDS")
 
 ui <- navbarPage(
     "Final Project",
@@ -57,6 +58,32 @@ ui <- navbarPage(
                     about it in the final-project channel in the Slack and 
                     am hoping to figure out what is going wrong!")
              ),
+    tabPanel("Modeling",
+             sidebarLayout(
+                 sidebarPanel(
+                     sliderInput("gini",
+                                 label = "Gini Score",
+                                 min = 0, max = 1, value = 0.5),
+                     sliderInput("number_protests",
+                                 label = "Total Number of Protests",
+                                 min = 0, max = 400, value = 50),
+                     sliderInput("poverty",
+                                 label = "Poverty Rate",
+                                 min = 0, max = 55, value = 12),
+                     sliderInput("income",
+                                 label = "Median Household Income",
+                                 min = 10000, max = 180000, value = 60000),
+                     sliderInput("population",
+                                 label = "Population",
+                                 min = 60000, max = 10000000, value = 150000),
+                     sliderInput("prop_less_than_hs",
+                                 label = "Proportion of Individuals with Less than HS Education",
+                                 min = 0, max = 0.3, value = 0.08)
+                 ),
+                 
+                 mainPanel(plotOutput("modelPlot"))
+             ),
+    ),
     tabPanel("Discussion"),
     tabPanel("About", 
              titlePanel("About"),
@@ -110,6 +137,31 @@ server <- function(input, output) {
             addTiles() %>%
             addCircleMarkers(lng = subset$longitude, lat = subset$latitude, radius = 0.1)
     })
+    
+    output$modelPlot <- renderPlot({
+        fit <- stan_glm(data = model_12_2,
+                        formula = number_violent ~ gini_score + number_protests + 
+                            poverty + income + population + prop_less_than_hs, 
+                        refresh = 0)
+        
+        new_obs <- tibble(gini_score = input$gini,
+                          number_protests = input$number_protests,
+                          poverty = input$poverty,
+                          income = input$income,
+                          population = input$population,
+                          prop_less_than_hs = input$prop_less_than_hs)
+        
+        posterior_predict(fit, newdata = new_obs) %>%
+            as_tibble() %>%
+            mutate_all(as.numeric) %>%
+            ggplot(aes(x = `1`)) +
+            geom_histogram(aes(y = after_stat(count/sum(count))), bins = 75,
+                           color = "white", position = "identity", alpha = 0.5) +
+            scale_y_continuous(labels = scales::percent_format()) + 
+            xlim(c(-5, 20)) + 
+            labs(x = "Number of Violent Protests")
+    })
+    
 }
 
 # Run the application 
