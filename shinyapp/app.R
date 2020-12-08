da2ff7f6a51e64b6c1b6d9f8db22817a68856c65
Victoria Wang <victoriawang@Victorias-MacBook-Pro.local>
2020-12-08 13:51:45 -0500

    added new comments to ShinyApp

--- shinyapp/app.R
+++ shinyapp/app.R
@@ -156,16 +156,16 @@ ui <- navbarPage(theme = shinytheme("sandstone"),
                       
                       p("Below is a brief description for each parameter:"),
                       tags$ul(
-                          tags$li(tags$b("Total Number of Protests"), "= number of protests, violent and non-violent, between May and October 2020"),
-                          tags$li(tags$b("Income Inequality"), "= economic inequality in a population, where higher values indicate greater inequality"),
-                          tags$li(tags$b("Poverty Rate"), "= poverty rate"),
-                          tags$li(tags$b("Severe Housing Problems Rate"), "= % households with at least 1 of 4 housing problems: overcrowding, high housing costs, or lack of kitchen or plumbing facilities"),
-                          tags$li(tags$b("Physical Distress Rate"), "= % adults reporting 14 or more days of poor physical health per month"),
-                          tags$li(tags$b("Mental Distress Rate"), "= % adults reporting 14 or more days of poor mental health per month"),
-                          tags$li(tags$b("Residential Segregation"), "= index of dissimilarity where higher values indicate greater residential segregation between non-White and White county residents."),
-                          tags$li(tags$b("Number of Police Killings"), "= number of murders by police officers"),
-                          tags$li(tags$b("% Black and Hispanic"), "= % Black and Hispanic"),
-                          tags$li(tags$b("Teen Birth Rate"), "= births per 100 females ages 15-19")
+                          tags$li(tags$b("Total Number of Protests (number_protests)"), "= number of protests, violent and non-violent, between May and October 2020"),
+                          tags$li(tags$b("Gini Index (gini_score)"), "= economic inequality in a population, where higher values indicate greater inequality"),
+                          tags$li(tags$b("Poverty Rate (poverty)"), "= poverty rate"),
+                          tags$li(tags$b("Severe Housing Problems Rate (housing_problems)"), "= % households with at least 1 of 4 housing problems: overcrowding, high housing costs, or lack of kitchen or plumbing facilities"),
+                          tags$li(tags$b("Physical Distress Rate (physical_distress)"), "= % adults reporting 14 or more days of poor physical health per month"),
+                          tags$li(tags$b("Mental Distress Rate (mental_distress)"), "= % adults reporting 14 or more days of poor mental health per month"),
+                          tags$li(tags$b("Residential Segregation (segregation_index_2)"), "= index of dissimilarity where higher values indicate greater residential segregation between non-White and White county residents."),
+                          tags$li(tags$b("Number of Police Killings (police_killings)"), "= number of murders by police officers"),
+                          tags$li(tags$b("% Black and Hispanic (percent_black_hispanic)"), "= % Black and Hispanic"),
+                          tags$li(tags$b("Teen Birth Rate (teen_birth_rate)"), "= births per 100 females ages 15-19")
                       )
 )))),
                           
@@ -198,26 +198,61 @@ server <- function(input, output) {
             filter(event_date %in% (input$daterange1[1]:input$daterange1[2])) %>%
             filter(sub_event_type == input$type)
         
+        # The previous lines of code create a subset of the ACLED dataset that 
+        # would reflect the choices the user makes in the type of protest
+        # and in the event date. 
+        
         leaflet(rural) %>%
             addTiles() %>%
             addCircles(lng = subset$longitude, lat = subset$latitude, 
-                       radius = 0.1, color = "navy")
+                       radius = 0.1, color = "navy", opacity = 0.2)
+        
+        # The longitude and latitude are set to come from subset, which is the 
+        # new dataset that is responsive to the user's options for type of 
+        # protest and date range. Opacity is set at 0.2 because the ACLED data
+        # provides longitude and latitude based on the city/county where 
+        # protests occurred, not the specific location — therefore, there are
+        # many overlapping points. Changing the opacity to be quite light allows
+        # us to see which areas actually had more protests. 
+        
     })
     
     output$protestPlot <- renderPlot({
         violence_plot <- function(ps){
             acled_data_11_12 %>%
+            
+            # acled_data_11_12 is a version of the ACLED data that was lightly
+            # cleaned in the data_gathering.Rmd file. 
+            
                 filter(sub_event_type %in% c("Peaceful protest", "Protest with intervention",
                                              "Violent demonstration", "Mob violence")) %>%
-                filter(subject == ps & group_boolean == TRUE) %>%
+            
+            # I filter for only protest activity — 'strategic developments', 
+            # 'attacks', and other sorts of unrest are not important for this 
+            # section. 
+            
+                filter(subject == ps & group_boolean == TRUE) %>% 
                 select(sub_event_type) %>%
                 group_by(sub_event_type) %>%
                 summarize(count = n(), .groups = "drop") %>%
+            
+            # This filters for the subject of the protest (which is indicated
+            # by 'ps', which will later be set as the input$plot_type as 
+            # defined in the ui section). It then counts the number of rows,
+            # grouped by the type of protest, which reflects the number of 
+            # protests of each type. 
+            
                 ggplot(aes(x = factor(sub_event_type, levels = c("Mob violence",
                                                                  "Violent demonstration",
                                                                  "Protest with intervention",
                                                                  "Peaceful protest")), 
                            y = count)) + 
+            
+            # I used factor and levels to sort the x axis variables in a way
+            # that made logical sense — they are sorted in order of the degree 
+            # of violence, with "Peaceful Protest" being the least escalated
+            # and "Mob Violence" the most. 
+            
                 geom_col(fill = "dodgerblue4", color = "white", alpha = 0.7) + 
                 labs(title = "Violence during Protests",
                      x = "Type of Protest",
@@ -230,6 +265,10 @@ server <- function(input, output) {
                                             "Violent \n demonstration",
                                             "Protest with \n intervention",
                                             "Peaceful \n protest")) + 
+            
+            # I drop all NA values and change the labels slightly for aesthetic
+            # purposes. 
+            
                 theme_economist() + 
                 coord_flip() + 
                 theme(axis.text.y = element_text(size = 10, hjust = 1),
@@ -240,17 +279,26 @@ server <- function(input, output) {
                                                   vjust = 4),
                       panel.background = element_rect(fill = "white"), 
                       plot.background = element_rect(fill = "white"))
+          
+          # Some modifications to make the plot prettier. 
+          
         }
         violence_plot(input$plot_type)
         })
     
     output$modelPlot <- renderPlot({
         fit <- stan_glm(data = model,
-                        formula = number_violent ~ teen_birth_rate + housing_problems + 
-                            physical_distress + mental_distress + segregation_index_2 + 
-                            police_killings + gini_score + number_protests + 
-                            poverty + percent_black_hispanic, 
+                        formula = number_violent ~ number_protests + 
+                          gini_score + poverty + housing_problems + 
+                          physical_distress + mental_distress + 
+                          segregation_index_2 + police_killings  + 
+                          percent_black_hispanic + teen_birth_rate,
                         refresh = 0)
+        
+        # Creating the actual model based off of cleaned data (ACLED data in 
+        # addition to parameter data) from the data_gathering.Rmd file. This 
+        # formula predicts the number of violent protests based on various
+        # other parameters. 
         
         new_obs <- tibble(number_protests = input$number_protests,
                           gini_score = input$gini_score,
@@ -263,22 +311,52 @@ server <- function(input, output) {
                           percent_black_hispanic = input$percent_black_hispanic,
                           teen_birth_rate = input$teen_birth_rate)
         
+        # I create a new_obs tibble that uses values that the user inputs using
+        # the slider function in the ui section. 
+        
         new <- posterior_predict(fit, newdata = new_obs) %>%
             as_tibble() %>%
             mutate_all(as.numeric) %>%
             rename("new" = `1`)
         
+        # I used posterior_predict because I am trying to predict the number of
+        # violent protests that would occur in a hypothetical county, rather
+        # than making statements about all counties in general. 
+        
         full <- default %>%
             bind_cols(new) %>%
+          
+          # "default" is a dataset that I created in the data_gathering.Rmd 
+          # file. It is essentially the same as the "new" object, except that 
+          # the inputs in newdata are the mean values for the dataset rather
+          # than the values that the user inputs. I chose to keep this dataset
+          # static so that the model wouldn't be rerunning the posterior_predict
+          # every single time the slider was altered. In addition, the general
+          # reason for including this default set is to allow for an easier
+          # comparison, making it easier to see how changing the input 
+          # parameters changes the number of violent protests. 
+          
+          # I then bind the "default" and "new" datasets together.
+          
             pivot_longer(cols = default:new,
                          names_to = "pp_results",
                          values_to = "number_violent")
+        
+        # pivot_longer() in this case is helpful only for the creation of the 
+        # ggplot. 
         
         full %>%
             ggplot(aes(x = number_violent, fill = pp_results)) +
             geom_histogram(aes(y = after_stat(count/sum(count))), bins = 75,
                            color = "white", position = "identity", alpha = 0.6) +
             scale_y_continuous(labels = scales::percent_format()) + 
+          
+          # I create a histogram with values from the full dataset, with 
+          # results from the posterior_predicts of our hypothetical county 
+          # ("new") as well as our mean county ("default"). The y = after_stat()
+          # changes the y-axis from a count to a probability, and the 
+          # percent_format() clarifies that it is a percentage. 
+          
             xlim(c(-10,20)) +
             labs(x = "Number of Violent Protests",
                  y = "Probability",
@@ -297,19 +375,30 @@ server <- function(input, output) {
                               breaks = c("default", "new"),
                               labels = c("Average County", "Hypothetical County"),
                               values = c("dodgerblue4", "darkgoldenrod"))
+        
+        # Aesthetic modifications. 
+        
     })
     output$regressionTable <- render_gt({
         fit <- stan_glm(data = model,
-                        formula = number_violent ~ teen_birth_rate + housing_problems + 
-                            physical_distress + mental_distress + segregation_index_2 + 
-                            police_killings + gini_score + number_protests + 
-                            poverty + percent_black_hispanic, 
+                        formula = number_violent ~ number_protests + 
+                          gini_score + poverty + housing_problems + 
+                          physical_distress + mental_distress + 
+                          segregation_index_2 + police_killings  + 
+                          percent_black_hispanic + teen_birth_rate,
                         refresh = 0)
+        
+        # This is the same fit model as the one in the previous section. 
         
         tbl_regression(fit, 
                        estimate_fun = function(x) style_sigfig(x, digits = 3)) %>%
             as_gt() %>%
             gt::tab_header(title = "Regression of Violent Protest Occurrence")
+        
+        # I used tbl_regression to create a nice table of results, which
+        # includes a 95% confident interval. I set the number of digits to 
+        # 3 because many of my values are quite small, given the generally 
+        # small number of violent protests that occur in any given county. 
     })
     
 }
